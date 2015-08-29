20372022eab8d8b9b04033979e8a72ced7140098
William Hsu <william7hsu@gmail.com>
2015-08-29 14:48:38 -0400

    JSON data

--- app/src/main/java/com/example/william/hackathonproject/getJson.java
+++ app/src/main/java/com/example/william/hackathonproject/getJson.java
@@ -1,43 +1,49 @@
 package com.example.william.hackathonproject;
 
-import android.util.Log;
-
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-public class getJSON {
-    public String getJSON(String address){
-        StringBuilder builder = new StringBuilder();
-        HttpClient client = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(address);
-        try{
-            HttpResponse response = client.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-                if(statusCode == 200){
-                HttpEntity entity = response.getEntity();
-                InputStream content = entity.getContent();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-                String line;
-                while((line = reader.readLine()) != null){
-                    builder.append(line);
-                }
-            } else {
-                Log.e(MainActivity.class.toString(), "Failedet JSON object");
-            }
-        }catch(ClientProtocolException e){
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class getJson {
+    public static void main(String[] args) throws IOException {
+        boolean isA = false;
+        boolean isB = true;
+        boolean isC = false;
+        String scode = "";
+        if (isA == true) {
+            scode = "TELEMETRY";
         }
-        return builder.toString();
+        if (isB == true) {
+            scode = "HARD_BRAKE";
+        }
+        StringBuilder urlBuilder = new StringBuilder("https://developer.gm.com/api/v1/fleet/vehicles/{vin}/data/services/{service_code}".replace("{vin}", URLEncoder.encode("1G6DH5E53C0000003", "UTF-8")).replace("{service_code}", URLEncoder.encode(scode, "UTF-8")));
+        urlBuilder.append("?");
+        urlBuilder.append(URLEncoder.encode("from","UTF-8") + "=" + URLEncoder.encode("2011-10-05T18:00:00.000Z", "UTF-8") + "&");
+        urlBuilder.append(URLEncoder.encode("to","UTF-8") + "=" + URLEncoder.encode("2014-10-05T19:00:00.000Z", "UTF-8") + "&");
+        urlBuilder.append(URLEncoder.encode("offet","UTF-8") + "=" + URLEncoder.encode("0", "UTF-8") + "&");
+        urlBuilder.append(URLEncoder.encode("limit","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/xml");
+        conn.setRequestProperty("Authorization", "Bearer 11accbbc-79a6-44d9-b3c9-d7657814cc84");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
     }
 }
